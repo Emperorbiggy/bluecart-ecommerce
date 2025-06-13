cbd2b2b4d4555483f928074f8fabd93973ae2a37
Emperorbiggy <rifelinktechnology@gmail.com>
2025-06-13 23:13:38 +0100

    Added users table

--- database/migrations/2025_06_13_221211_add_profile_fields_to_users_table.php
+++ database/migrations/2025_06_13_221211_add_profile_fields_to_users_table.php
@@ -4,25 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('phone')->nullable();
+            $table->string('profile_image')->nullable();
+            $table->text('billing_address')->nullable();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn(['phone', 'profile_image', 'billing_address']);
         });
     }
 };
