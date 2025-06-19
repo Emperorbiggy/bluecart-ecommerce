3dd00338718ff049b7d0f6f7b79738401e92c6c5
Emperorbiggy <rifelinktechnology@gmail.com>
2025-06-19 10:33:40 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/bluecart/',
-  build: {
-    outDir: 'build', // move it out of public
-    emptyOutDir: true,
-  },
-  plugins: [
-    laravel({
-      input: 'resources/js/app.jsx',
-      refresh: true,
-    }),
-    react(),
-  ],
-})
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            refresh: true,
+        }),
+        react(),
+    ],
+});
