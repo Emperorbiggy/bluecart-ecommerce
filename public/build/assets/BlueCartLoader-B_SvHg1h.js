47ad70c0b028b39a9dfdc2d30beed8c1e59c47d7
Emperorbiggy <rifelinktechnology@gmail.com>
2025-06-19 09:49:10 +0100

    fix redirect bug

---
+++ public/build/assets/BlueCartLoader-B_SvHg1h.js
@@ -0,0 +1,16 @@
+import{c as n}from"./createLucideIcon-jFZSC4aP.js";import{r as c,j as e}from"./app-D95Z1pnu.js";/**
+ * @license lucide-react v0.515.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"M4 12h16",key:"1lakjw"}],["path",{d:"M4 18h16",key:"19g7jn"}],["path",{d:"M4 6h16",key:"1o0s65"}]],p=n("menu",r);/**
+ * @license lucide-react v0.515.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const x=[["circle",{cx:"8",cy:"21",r:"1",key:"jimo8o"}],["circle",{cx:"19",cy:"21",r:"1",key:"13723u"}],["path",{d:"M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12",key:"9zh506"}]],u=n("shopping-cart",x);/**
+ * @license lucide-react v0.515.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]],f=n("x",h),b=()=>{const s="BlueCart",[a,o]=c.useState(""),[t,i]=c.useState(0);return c.useEffect(()=>{if(t<s.length){const l=setTimeout(()=>{o(s.slice(0,t+1)),i(t+1)},150);return()=>clearTimeout(l)}},[t,s]),e.jsx("div",{className:"flex flex-col items-center justify-center h-screen bg-white",children:e.jsxs("div",{className:"flex items-center gap-2 animate-pulse",children:[e.jsx(u,{className:"text-osunblue w-8 h-8 animate-bounce"}),e.jsxs("h1",{className:"text-3xl font-bold tracking-wide text-osunblue flex items-center",children:[a,e.jsx("span",{className:"ml-1 w-[2px] h-6 bg-osunblue animate-blink"})]})]})})};export{b as B,p as M,u as S,f as X};
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/bluecart/', 
+    base: '/bluecarts/', 
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
