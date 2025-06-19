fa0eebef548242f37721ec6ec5dd528a1307402e
Emperorbiggy <rifelinktechnology@gmail.com>
2025-06-19 10:16:04 +0100

    rebuilding

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/bluecart/', // ✅ This ensures all assets load from the subfolder
+  base: '/bluecart/',
+  build: {
+    outDir: 'build', // move it out of public
+    emptyOutDir: true,
+  },
   plugins: [
     laravel({
       input: 'resources/js/app.jsx',
       refresh: true,
-      buildDirectory: 'build', // ✅ important if you changed default
     }),
     react(),
   ],
-});
-
+})
